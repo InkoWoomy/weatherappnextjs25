694a2522a64e61746383f884ca1f6de6d70c3342
Alexander <yoda4115@gmail.com>
2025-03-21 15:16:33 -0700

    redeployattempt

---
+++ src/components/FavoritesListItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FavoritesListItem = () => {
+  return (
+    <div className='col-span-5 bg-gray-300 m rounded-md'>
+        <h3>{}</h3>
+    </div>
+  )
+}
+
+export default FavoritesListItem

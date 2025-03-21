be2b77e9c85c1f69142876ecd45d994ea1d4d0f8
Alexander <yoda4115@gmail.com>
2025-03-21 15:36:29 -0700

    ---

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,22 +1,23 @@
 import React from 'react'
-import { APIKEY } from './enviroment'
+
+const apikey = process.env.NEXT_PUBLIC_MY_API_KEY; 
 
 //GetLocation for searching location. Important for the next functions
 export const GetLocation = async(city: string) => {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKEY}`);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`);
     const data = await response.json();
     return data;
 }
 
 //GetCurrent Day to get the current location (will reguire that the latitude and longtitude is assigned to their own variables before calling)
 export const GetCurrentDay = async(lat: number, lon: number) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=imperial`)
     const data = await response.json();
     return data;
 }
 
 export const GetFiveDay = async(lat: number, lon: number) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}&units=imperial`)
     const data = await response.json();
     return data;
 }
